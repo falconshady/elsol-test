1f56fb224bb3dfc511fff4eac6ab35f5c7b8354f
falconshady <falconshady@gmail.com>
2024-01-23 04:17:43 -0500

    update store resources

--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+import { Store } from "./entities/store.entity";
+
 import { CreateStoreDto } from './dto/create-store.dto';
 import { UpdateStoreDto } from './dto/update-store.dto';
 
 @Injectable()
 export class StoreService {
+  constructor(
+    @InjectRepository(Store)
+    private storesRepository: Repository<Store>,
+  ) {}
   create(createStoreDto: CreateStoreDto) {
     return 'This action adds a new store';
   }
 
   findAll() {
-    return `This action returns all store`;
+    return this.storesRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} store`;
+    return this.storesRepository.findOneBy({ id });
   }
 
   update(id: number, updateStoreDto: UpdateStoreDto) {
     return `This action updates a #${id} store`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} store`;
+  async remove(id: number): Promise<void> {
+    await this.storesRepository.delete(id);
   }
 }
